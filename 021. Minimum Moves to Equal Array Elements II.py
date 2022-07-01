d2a8b5bfdcd9b2b30e2861241e0e32e4be2012d6
DivyanshiChouksey <89761026+DivyanshiChouksey@users.noreply.github.com>
2022-07-01 22:17:40 +0530

    Create 021. Minimum Moves to Equal Array Elements II.py

---
+++ 021. Minimum Moves to Equal Array Elements II.py
@@ -0,0 +1,20 @@
+# Minimum Moves to Equal Array Elements II
+"""
+    sort the array, find the median of the array and then
+    calculate steps taken to make every element of array equals to the median of the array,
+    finally return the total steps taken.
+"""
+
+# Time Complexity = O(nlog) || Space Complexity = O(1)
+nums = [1,2,3]
+
+def minMoves2(nums): 
+    nums.sort()
+    mid = len(nums)//2
+    moves = 0
+    for i in range(len(nums)):
+        moves += abs(nums[i] - nums[mid])
+
+    return moves
+
+print(minMoves2(nums))
