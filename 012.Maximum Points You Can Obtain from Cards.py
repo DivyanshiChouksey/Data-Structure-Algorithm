f33db3830e06c3fc7cbebeed3f0f234a2d325b8a
DivyanshiChouksey <89761026+DivyanshiChouksey@users.noreply.github.com>
2022-07-13 23:32:22 +0530

    Update 012.Maximum Points You Can Obtain from Cards.py

--- 012.Maximum Points You Can Obtain from Cards.py
+++ 012.Maximum Points You Can Obtain from Cards.py
@@ -8,28 +8,30 @@
     [1,79,80,1,1,1,200,1]   now compare and store the sum of k-1 cardpoint from beginning &
      * *               *    1 cardpint from last to the pervious points = 160, (81<160) 
 
-    [1,79,80,1,1,1,200,1]   again compare and store the sum of k-1 cardpoint from beginning &
-     *              *  *    1 cardpint from last to the pervious points = 202, (160<202) 
+    [1,79,80,1,1,1,200,1]   again compare and store the sum of k-2 cardpoint from beginning &
+     *              *  *    2 cardpint from last to the pervious points = 202, (160<202) 
     
     repeat the working until k > 0 and return the max points
     
 """
 # Time Complexity = O(n) || Space Complexity = O(1)
 
-cardPoints = [1,79,80,1,1,1,200,1]
+cardPoints = [1, 79, 80, 1, 1, 1, 200, 1]
 k = 3
+
 
 def maxPoint(cardPoints, k):
     i = 0
-    j = len(cardPoints)-1
+    j = len(cardPoints) - 1
     total = sum(cardPoints[:k])
     best = total
     while k > 0:
-        total += cardPoints[j] - cardPoints[k-1]
+        total += cardPoints[j] - cardPoints[k - 1]
         k -= 1
         j -= 1
         if total > best:
             best = total
     return best
 
-print(maxPoint(cardPoints,k))
+
+print(maxPoint(cardPoints, k))
