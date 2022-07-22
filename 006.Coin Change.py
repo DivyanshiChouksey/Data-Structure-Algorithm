03f35cdc233c281c3ea7ef90374c84da0e9fad5a
DivyanshiChouksey <89761026+DivyanshiChouksey@users.noreply.github.com>
2022-07-22 23:38:12 +0530

    Update 006.Coin Change.py

--- 006.Coin Change.py
+++ 006.Coin Change.py
@@ -9,17 +9,19 @@
 """
 # Time Complexity = O(m*n) || Space Complexity = O(n)
 
-coins = [1,2,5]
+coins = [1, 2, 5]
 amount = 11
 
-def coinChange(coins,amount):
-    dp = [float("inf") for i in range(amount+1)]
+
+def coinChange(coins, amount):
+    dp = [float("inf") for i in range(amount + 1)]
     dp[0] = 0
     for coin in coins:
-        for i in range(coin,amount+1):
-            dp[i] = min(dp[i],dp[i-coin]+1)
+        for i in range(coin, amount + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
     if dp[-1] == float("inf"):
         return -1
     return dp[-1]
 
-print(coinChange(coins,amount))
+
+print(coinChange(coins, amount))
