ea87063b6ebb869334729ac85a12dd35ab04e794
DivyanshiChouksey <89761026+DivyanshiChouksey@users.noreply.github.com>
2022-09-19 22:58:40 +0530

    Update 211.Depth First Search.py

--- 211.Depth First Search.py
+++ 211.Depth First Search.py
@@ -1,15 +1,12 @@
 # Depth First Search
 
 """
-
+    Start with the very first branch of the given graph(here tree) 
+    and explore as much as depth you can then repeat for the next branch and so on.
 """
 
 # Time Complexity = O(v+e) , e = edges
 # Space Complexity = O(v),   v = vertices
-
-"""
-    we have to explore first branch go through it as much as depth we have 
-"""
 
 
 class Node:
@@ -18,7 +15,7 @@ class Node:
         self.name = name
 
     def addChild(self, name):
-        self.children.append(Node(name))
+        self.children.append(name)
         return self
 
     def depthFirstSearch(self, array):
@@ -26,3 +23,33 @@ class Node:
         for child in self.children:
             child.depthFirstSearch(array)
         return array
+
+
+A = Node("A")
+B = Node("B")
+C = Node("C")
+D = Node("D")
+E = Node("E")
+F = Node("F")
+G = Node("G")
+H = Node("H")
+I = Node("I")
+J = Node("J")
+K = Node("K")
+
+for i in [B, C, D]:
+    A.addChild(i)
+
+for i in [E, F]:
+    B.addChild(i)
+
+for i in [G, H]:
+    D.addChild(i)
+
+for i in [I, J]:
+    F.addChild(i)
+
+for i in [K]:
+    G.addChild(i)
+
+print(A.depthFirstSearch([]))
