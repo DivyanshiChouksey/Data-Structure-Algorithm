05b3a887d4c44ff75b2e0fa189c6f3af4cc9580f
DivyanshiChouksey <89761026+DivyanshiChouksey@users.noreply.github.com>
2022-10-06 20:43:41 +0530

    Create 252.Odd Even Linked List.py

---
+++ 252.Odd Even Linked List.py
@@ -0,0 +1,44 @@
+# Odd Even Linked List
+
+# Time Complexity = O(n) || Space Complexity = O(1)
+class LinkedList:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+head = LinkedList(1)
+n1 = LinkedList(2)
+n2 = LinkedList(3)
+n3 = LinkedList(4)
+n4 = LinkedList(5)
+
+
+head.next = n1
+n1.next = n2
+n2.next = n3
+n3.next = n4
+
+
+def oddEvenList(head):
+
+    if not head:
+        return head
+    odd = head
+    even = head.next
+    eHead = even
+
+    while even and even.next:
+        odd.next = odd.next.next
+        even.next = even.next.next
+        odd = odd.next
+        even = even.next
+
+    odd.next = eHead
+    return head
+
+
+newHead = oddEvenList(head)
+while newHead:
+    print(newHead.value)
+    newHead = newHead.next
