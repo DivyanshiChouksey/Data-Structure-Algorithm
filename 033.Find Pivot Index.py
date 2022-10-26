08211d75a9573158b83261f8e5dca9d2f37fc66f
DivyanshiChouksey <89761026+DivyanshiChouksey@users.noreply.github.com>
2022-10-26 22:17:51 +0530

    Update 033.Find Pivot Index.py

--- 033.Find Pivot Index.py
+++ 033.Find Pivot Index.py
@@ -1,9 +1,17 @@
 # Find Pivot Index
-"""
-"""
-# Time Complexity = O() || Space Complexity = O()
 
-nums = [1,7,3,6,5,6]
+"""
+    Start with taking totalSum and a leftsum initially zero
+    then iterate throgh the array, and for every current value
+    the rightSum would be substract leftSum and current value from total Sum 
+    and check rightSum and leftSum are equal then return current value index else add current value to 
+    leftSum after that again do the same step for next value
+    and at the end if leftSum is not equal to rightSum then return -1
+"""
+# Time Complexity = O(n) || Space Complexity = O(1)
+
+nums = [1, 7, 3, 6, 5, 6]
+
 
 def pivotIdx(nums):
     totalSum = sum(nums)
@@ -14,5 +22,6 @@ def pivotIdx(nums):
             return i
         leftSum += nums[i]
     return -1
+
 
 print(pivotIdx(nums))
