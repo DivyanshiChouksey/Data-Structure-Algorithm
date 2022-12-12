5ada8ee65def895213c778ad0c0f8b93b6e25256
DivyanshiChouksey <89761026+DivyanshiChouksey@users.noreply.github.com>
2022-12-12 21:07:38 +0530

    Update 171.Average of Levels in Binary Tree.py

--- 171.Average of Levels in Binary Tree.py
+++ 171.Average of Levels in Binary Tree.py
@@ -1,21 +1,66 @@
 # Average of Levels in Binary Tree
 
-root = 3
 
-stack = []
-ans = []
-node = root
-while node:
-    tmp = []
-    total = 0
-    for node in stack:
-        total += node.val
-        if node.left:
-            tmp.append(node.left)
-        if node.right:
-            tmp.append(node.right)
+class TreeNode:
+    def __init__(self, val, left=None, right=None):
+        """
+        if this was not a binary tree then we would have an array of children
+        """
+        self.val = val
+        self.left = left
+        self.right = right
 
-    ans.append(total / len(stack))
-    stack = tmp[:]
 
-print(ans)
+"""
+                3
+            --------
+            9      20
+            
+                 --------
+                 15     7
+
+"""
+
+node11 = TreeNode(3)
+node12 = TreeNode(9)
+node13 = TreeNode(20)
+node14 = TreeNode(15)
+node15 = TreeNode(7)
+
+root = node11
+node11.left = node12
+node11.right = node13
+node13.left = node14
+node13.right = node15
+
+
+"""
+    Do Level Order Traversal and find average of each level and append it to the ans 
+    return the ans 
+"""
+
+# Time Complexity = O(n) || Space Complexity = O(n)
+
+
+def averageOfLevels(root):
+    # level order traversal
+    stack = [root]
+    ans = []
+    node = root
+    while stack:
+        tmp = []
+        total = 0
+        for node in stack:
+            total += node.val
+            if node.left:
+                tmp.append(node.left)
+            if node.right:
+                tmp.append(node.right)
+
+        ans.append(total / len(stack))
+        stack = tmp[:]
+
+    return ans
+
+
+print(averageOfLevels(root))
