8dd92457f825924ae17e10818c14138344158e32
DivyanshiChouksey <89761026+DivyanshiChouksey@users.noreply.github.com>
2022-12-24 23:05:29 +0530

    Create 330.Domino and Tromino Tiling.py

---
+++ 330.Domino and Tromino Tiling.py
@@ -0,0 +1,16 @@
+# Domino and Tromino Tiling
+
+# Time Complexity = O(n) || Space Complexity = O(n)
+n = 3
+
+
+def numTilings(n):
+    dp = [[0, 0] for _ in range(3)]
+    dp[1], dp[2] = [1, 1], [2, 2]
+    for i in range(3, n + 1):
+        dp[i % 3][0] = dp[(i - 1) % 3][0] + dp[(i - 2) % 3][0] + 2 * dp[(i - 2) % 3][1]
+        dp[i % 3][1] = dp[(i - 1) % 3][0] + dp[(i - 1) % 3][1]
+    return dp[n % 3][0] % 1_000_000_007
+
+
+print(numTilings(n))
