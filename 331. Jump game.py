8e64fca111a51243842ceabf256abf767fc1ff4a
DivyanshiChouksey <89761026+DivyanshiChouksey@users.noreply.github.com>
2022-12-26 23:13:52 +0530

    Create 331. Jump game.py

---
+++ 331. Jump game.py
@@ -0,0 +1,29 @@
+# Jump Game
+
+"""
+    Greedy solution 
+    We start from the back of array and then 
+    keep the track of maximum distance we can go from that index 
+    if (i+nums[i]) >= goal then update goal to i
+    and if our goal is zero that means we cannot go after that point so return False.
+    
+"""
+
+# Time Complexity = O(n)|| Space Complexity = O(1)
+
+nums = [2, 3, 1, 1, 4]
+
+
+def jumpGame(nums):
+    goal = len(nums) - 1
+
+    for i in range(len(nums) - 1, -1, -1):
+        if i + nums[i] >= goal:
+            goal = i
+    if goal == 0:
+        return True
+    else:
+        return False
+
+
+print(jumpGame(nums))
