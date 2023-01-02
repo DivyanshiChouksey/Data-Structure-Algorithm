5aac03dc50d3667dbae04612c63a66eb2f410a48
DivyanshiChouksey <89761026+DivyanshiChouksey@users.noreply.github.com>
2023-01-02 23:39:34 +0530

    Update 336.Word Pattern.py

--- 336.Word Pattern.py
+++ 336.Word Pattern.py
@@ -1,6 +1,15 @@
 # Word Pattern
 
-# Time Complexity = O() || Space Complexity = O()
+"""
+    Creating a hash map to associate each and unqiue character of pattern to unique word of s 
+    for this split the s to get the access of separate words in s
+    after that go through the pattern and s string simultaneously with the help of zip funtion
+    if the character is already having a distinct word associated with it or a word having
+    more than one character associated with it then return False otherwise return True
+    For early stoping check the length of both string.
+"""
+
+# Time Complexity = O(n) || Space Complexity = O(n)
 pattern = "abba"
 s = "dog cat cat dog"
 
