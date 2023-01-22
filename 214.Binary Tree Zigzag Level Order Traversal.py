ca97fac98ef1392124a4dec42409f33e16b307e9
DivyanshiChouksey <89761026+DivyanshiChouksey@users.noreply.github.com>
2023-01-22 23:40:38 +0530

    Update 214.Binary Tree Zigzag Level Order Traversal.py

--- 214.Binary Tree Zigzag Level Order Traversal.py
+++ 214.Binary Tree Zigzag Level Order Traversal.py
@@ -14,26 +14,45 @@ node2 = TreeNode(20)
 node3 = TreeNode(15)
 node4 = TreeNode(7)
 
+root.left = node1
+root.right = node2
+node2.left = node3
+node2.right = node4
 
-if root is None:
-    print("[]")
-ans = []
-level = 1
-queue = [root]
-while queue:
-    tmpans = []
-    tmp = []
-    for node in queue:
-        tmpans.append(node.val)
-        if node.left:
-            tmp.append(node.left)
-        if node.right:
-            tmp.append(node.right)
-    queue = tmp[:]
-    if level % 2 == 0:
-        ans.append(tmpans[::-1])
-    else:
-        ans.append(tmpans)
-    level += 1
-# return ans
-print(ans)
+
+# Iterative Solution
+
+"""
+    Simple traverse the binary tree by level-order traversal (bfs) 
+    but reverse alternate levels befor adding them to the ans  
+"""
+
+# Time Complexity = O(n) || Space Complexity = O(n)
+
+
+def zigzagLevelOrder(root):
+    if not root:
+        return []
+    level = 1
+    ans = []
+    queue = [root]
+    while queue:
+        tmp = []
+        tmpans = []
+        for node in queue:
+            tmpans.append(node.val)
+            if node.left:
+                tmp.append(node.left)
+            if node.right:
+                tmp.append(node.right)
+        queue = tmp[:]
+        if level % 2 == 0:
+            ans.append(tmpans[::-1])
+        else:
+            ans.append(tmpans)
+
+        level += 1
+    return ans
+
+
+print(zigzagLevelOrder(root))
