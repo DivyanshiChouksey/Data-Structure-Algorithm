e8dee451f13ae611c2bfa362bb5dccdf4d402a68
Divyanshi Chouksey <89761026+DivyanshiChouksey@users.noreply.github.com>
2023-02-25 20:29:25 +0530

    Create 433.Best Time to Buy and Sell Stock.py

---
+++ 433.Best Time to Buy and Sell Stock.py
@@ -0,0 +1,21 @@
+# Best Time to Buy and Sell Stock
+
+# Time Complexity = O(n) || Space Complexity = O(1)
+
+prices = [7,1,5,3,6,4]
+
+def maxProfit(prices):
+    l, r = 0,1
+    maxP = 0
+    while r<len(prices):
+        if prices[l] < prices[r]:
+            profit = prices[r] - prices[l]
+            maxP = max(maxP, profit)
+        else:
+            l = r
+        r +=1
+        
+    return maxP
+            
+
+print(maxProfit(prices))
