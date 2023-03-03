c4b499feeafcc1bb6927632eee5aa8de9a3ffe4f
Divyanshi Chouksey <89761026+DivyanshiChouksey@users.noreply.github.com>
2023-03-03 20:38:05 +0530

    Update 438.String Compression.py

--- 438.String Compression.py
+++ 438.String Compression.py
@@ -1,11 +1,26 @@
 # String Compression
 
 """
-    Using sliding window to count the frequency and make inplace change in chars array
-    at i+1 index and for next character do the same at sequence indeces. 
+    Using sliding window approach and for optimization modifying the input array
+    taking two pointer i, j initially at 0th index then by using sliding window count the 
+    group of consecutive repeating characters after that first place the character by updating chars[i] = chars[j]
+    and then for count check if count is bigger than 1 then at (i+1)th indeces 
+    update the character with the count (digit by digit) of ith indexed character else if count is 1 then 
+    place it at the jth index and move to the next at the end return the index of i for the length of compressed 
+    string.
+
+Note - compressing the string and updating it inplace from 0th index
+       example:-                                 i                                    j
+                            ["a","a","a""b","b","b","b","b","b","b","b","b","b","b","b"]
+                              0   1   2  3   4   5   6   7
+                compressed str - ["a","3","b","1","2"]
+
+                length of compressed string would be 5
 """
 
 # Time Complexity = O(n) || Space Complexity = O(1)
+
+chars = ["a","b","b","b","b","b","b","b","b","b","b","b","b"]
 
 def compress(chars):
     i=0
@@ -24,3 +39,5 @@ def compress(chars):
         j+=1
         i+=1
     return i
+
+print(compress(chars))
