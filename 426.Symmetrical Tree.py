1b632986f038a17213cd7464b3e5746eaf74206f
Divyanshi Chouksey <89761026+DivyanshiChouksey@users.noreply.github.com>
2023-03-05 23:44:24 +0530

    Update 426.Symmetrical Tree.py

--- 426.Symmetrical Tree.py
+++ 426.Symmetrical Tree.py
@@ -1,5 +1,46 @@
 # Symmetrical Tree
 
+class TreeNode:
+    def __init__(self, val, left=None, right=None):
+        
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+node1 = TreeNode(1)
+node2 = TreeNode(2)
+node3 = TreeNode(2)
+node4 = TreeNode(3)
+node5 = TreeNode(3) 
+node6 = TreeNode(4)
+node7 = TreeNode(4)
+node8 = TreeNode(5)
+node9 = TreeNode(5)
+node10 = TreeNode(6) 
+node11 = TreeNode(6) 
+
+
+"""
+Initailize two stack stackLeft with root.left node in it and stackRight with root.right node in it then pop from
+both stack and store it in left and right variable respectively then check if popped node are not equal then return 
+false after that for symmerty or we can say to keep record
+of mirror image we have to check root's rightsubtree is the invert of root's leftsubtree
+the tricky part is that in left stack we push left node first then right node second of popped node from left stack
+and in right stack we push right node first then left node second of popped node from right and repeat the process
+at the end return True 
+
+
+                  1
+            ------------
+            2          2
+        -------     -------
+        3     4     4     3  
+     ------             ------   
+     5    6             5    6
+                     
+"""
+
 class BinaryTree:
     def __init__(self, value, left=None, right=None):
         self.value = value
