e5ea498c1c4f8a8ba368b0a0ec1a578dcf3ffc39
Divyanshi Chouksey <89761026+DivyanshiChouksey@users.noreply.github.com>
2023-03-07 08:56:01 +0530

    Update 439.Kth Missing Positive Number.py

--- 439.Kth Missing Positive Number.py
+++ 439.Kth Missing Positive Number.py
@@ -1,17 +1,16 @@
 # Kth Missing Positive Number
 
-l, r = 0, len(A)
+A = [2,3,4,7,11]
+k = 5
 
-        while l < r:
+def findKthPositive( A, k):
+    l, r = 0, len(A)
+    while l < r:
+        m = (l + r) // 2
+        if A[m] - 1 - m < k:
+            l = m + 1
+        else:
+            r = m
+    return l + k 
 
-            m = (l + r) / 2
-
-            if A[m] - 1 - m < k:
-
-                l = m + 1
-
-            else:
-
-                r = m
-
-        return l + k
+print(findKthPositive( A, k))
