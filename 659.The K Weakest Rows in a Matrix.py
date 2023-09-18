78860584097dae949b92d466778ac2901744254b
Divyanshi Chouksey <89761026+DivyanshiChouksey@users.noreply.github.com>
2023-09-18 23:51:31 +0530

    Create 659.The K Weakest Rows in a Matrix.py

---
+++ 659.The K Weakest Rows in a Matrix.py
@@ -0,0 +1,27 @@
+# The K Weakest Rows in a Matrix
+
+mat = 
+[[1,1,0,0,0],
+ [1,1,1,1,0],
+ [1,0,0,0,0],
+ [1,1,0,0,0],
+ [1,1,1,1,1]], 
+k = 3
+
+def kWeakestRows(mat,k):
+    heap = []
+    for i in range(len(mat)):
+        tmp = 0
+        for j in range(len(mat[i])):
+            if mat[i][j] ==1:
+                tmp+=1
+        heapq.heappush(heap,(tmp,i))
+    
+    ans = []
+    for i in range(k):
+        ans.append(heapq.heappop(heap)[1])
+    
+    return ans 
+
+
+print(kWeakestRows(mat,k))
