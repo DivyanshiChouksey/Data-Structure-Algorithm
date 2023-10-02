948d6b8b499503696ed31145eab15a6ee3bc3e5c
Divyanshi Chouksey <89761026+DivyanshiChouksey@users.noreply.github.com>
2023-10-02 22:34:55 +0530

    Create 671.Remove Colored Pieces if Both Neighbors are the Same Color.py

---
+++ 671.Remove Colored Pieces if Both Neighbors are the Same Color.py
@@ -0,0 +1,19 @@
+# Remove Colored Pieces if Both Neighbors are the Same Color
+
+colors = "AAABABB"
+
+
+def winnerOfGame(colors):
+    alice = 0
+    bob = 0
+    
+    for i in range(1, len(colors) - 1):
+        if colors[i - 1] == colors[i] == colors[i + 1]:
+            if colors[i] == "A":
+                alice += 1
+            else:
+                bob += 1
+    
+    return alice - bob >= 1
+
+print( winnerOfGame(colors))
