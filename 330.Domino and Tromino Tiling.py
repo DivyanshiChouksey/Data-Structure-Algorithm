8be637c18e294819ec30af6f24359aa6b093098e
Divyanshi Chouksey <89761026+DivyanshiChouksey@users.noreply.github.com>
2023-10-13 22:47:34 +0530

    Update 330.Domino and Tromino Tiling.py

--- 330.Domino and Tromino Tiling.py
+++ 330.Domino and Tromino Tiling.py
@@ -13,4 +13,23 @@ def numTilings(n):
     return dp[n % 3][0] % 1_000_000_007
 
 
+
+# Solution 2
+
+n=3
+def numTilings(n):
+    # 1 1 2 5 11 24 53 117 0 
+    if n<2:
+        return 1
+
+    dp = [0 for _ in range(n+1)]
+    dp[0] = dp[1]= 1
+    dp[2] = 2
+    for i in range(3,n+1):
+        dp[i] = (dp[i-1]*2)+dp[i-3]
+    # print(dp)
+    return (dp[-1]) % ((10**9)+7)
+
+
+print(numTilings(n))
 print(numTilings(n))
