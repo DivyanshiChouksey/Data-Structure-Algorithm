515631cc9894781078c8307cfb7b117cbc4dda52
DivyanshiChouksey <choukseydivyanshi@gmail.com>
2023-10-16 23:26:06 +0530

    Update 672.Number of Good Pairs.py

--- 672.Number of Good Pairs.py
+++ 672.Number of Good Pairs.py
@@ -6,7 +6,9 @@ nums = [1,2,3,1,1,3]
 There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
 """
 
-def numIdenticalPairs(nums):
+from collections import defaultdict
+
+def numIdenticalPairs(nums):  
     ans = 0
     dct =  defaultdict(int)
     for num in nums:
