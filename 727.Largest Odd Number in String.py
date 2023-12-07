02a4a60852856a229aff24c2b8fd9a2a7396222d
Divyanshi Chouksey <89761026+DivyanshiChouksey@users.noreply.github.com>
2023-12-07 23:15:38 +0530

    Create 727.Largest Odd Number in String.py

---
+++ 727.Largest Odd Number in String.py
@@ -0,0 +1,13 @@
+# Largest Odd Number in String
+
+num = "52"
+
+def largestOddNumber(num):
+    for i in range(len(num) - 1, -1, -1):
+        if int(num[i]) % 2 != 0:
+            return num[:i + 1]
+        
+    return ""
+
+
+print(largestOddNumber(num))
