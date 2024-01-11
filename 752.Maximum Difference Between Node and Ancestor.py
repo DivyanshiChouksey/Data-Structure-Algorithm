f9500caf9250eaedb4b1c9d82eb90e44ef28c9d6
Divyanshi Chouksey <89761026+DivyanshiChouksey@users.noreply.github.com>
2024-01-11 22:23:34 +0530

    Create 752.Maximum Difference Between Node and Ancestor.py

---
+++ 752.Maximum Difference Between Node and Ancestor.py
@@ -0,0 +1,37 @@
+# Maximum Difference Between Node and Ancestor
+
+"""
+Input: root = [8,3,10,1,6,null,14,null,null,4,7,13]
+Output: 7
+Explanation: We have various ancestor-node differences, some of which are given below :
+|8 - 3| = 5
+|3 - 7| = 4
+|8 - 1| = 7
+|10 - 13| = 3
+Among all possible differences, the maximum value of 7 is obtained by |8 - 1| = 7.
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxAncestorDiff(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+
+        def helper(node, cur_max, cur_min):
+            # if encounter leaves, return the max-min along the path
+            if not node:
+                return cur_max - cur_min
+            # else, update max and min
+            # and return the max of left and right subtrees
+            cur_max = max(cur_max, node.val)
+            cur_min = min(cur_min, node.val)
+            left = helper(node.left, cur_max, cur_min)
+            right = helper(node.right, cur_max, cur_min)
+            return max(left, right)
+
+        return helper(root, root.val, root.val)
