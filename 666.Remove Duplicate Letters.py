0766a4766728ee210d6d3995b26ba20204d1dd52
DivyanshiChouksey <choukseydivyanshi@gmail.com>
2024-01-17 19:27:07 +0530

    Update 666.Remove Duplicate Letters.py

--- 666.Remove Duplicate Letters.py
+++ 666.Remove Duplicate Letters.py
@@ -17,3 +17,30 @@ def removeDuplicateLetters(self, s: str) -> str:
                         break
                 stack.append(s[i])
         return "".join(stack)
+
+
+
+# Remove Duplicate Letters
+
+from collections import defaultdict
+
+s = "cbacdcbc"
+# s = ""
+
+dct = defaultdict(int)
+for i in range(len(s)):
+    dct[s[i]] = i
+
+print(dct)
+
+stack = []
+for i in range(len(s)):
+    if s[i] not in stack:
+        while stack and stack[-1] > s[i]:
+            if i < dct[stack[-1]]:
+                stack.pop()
+            else:
+                break
+        stack.append(s[i])
+
+print("".join(stack))
