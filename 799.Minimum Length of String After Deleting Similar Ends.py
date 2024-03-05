18191301e884b895f5c1cb304fe8a5c579c0d40f
Divyanshi Chouksey <89761026+DivyanshiChouksey@users.noreply.github.com>
2024-03-05 23:17:30 +0530

    Create 799.Minimum Length of String After Deleting Similar Ends.py

---
+++ 799.Minimum Length of String After Deleting Similar Ends.py
@@ -0,0 +1,19 @@
+# Minimum Length of String After Deleting Similar Ends
+
+
+s = "aabccabba"
+
+def minimumLength(s):
+    left, right = 0, len(s) - 1
+    
+    while left < right and s[left] == s[right]:
+        char = s[left]
+        while left <= right and s[left] == char:
+            left += 1
+        while right >= left and s[right] == char:
+            right -= 1
+    
+    return right - left + 1
+
+
+print(minimumLength(s))
