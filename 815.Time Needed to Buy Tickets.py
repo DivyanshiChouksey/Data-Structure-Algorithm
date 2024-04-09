53439ca700ea3945116111b77ebc43ac72931c20
Divyanshi Chouksey <89761026+DivyanshiChouksey@users.noreply.github.com>
2024-04-09 23:52:25 +0530

    Create 815.Time Needed to Buy Tickets.py

---
+++ 815.Time Needed to Buy Tickets.py
@@ -0,0 +1,13 @@
+# Time Needed to Buy Tickets
+
+tickets = [4,3,5,7]
+
+class Solution:
+    def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
+        ans = 0
+        for i in range(len(tickets)):
+            if i<=k:
+                ans += min(tickets[k],tickets[i])
+            else:
+                ans += min(tickets[k]-1,tickets[i])
+        return ans
