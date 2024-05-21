37e888a62fb659ef4e5ad212e22f40a61b6ff457
Divyanshi Chouksey <89761026+DivyanshiChouksey@users.noreply.github.com>
2024-05-21 23:50:48 +0530

    Create 837.Subsets.py

---
+++ 837.Subsets.py
@@ -0,0 +1,8 @@
+# Subsets
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        result = [[]]
+        for num in nums:
+            result += [curr + [num] for curr in result]
+        return result
