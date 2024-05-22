62436f8f7d4548a87419387137cb146a15c09355
Divyanshi Chouksey <89761026+DivyanshiChouksey@users.noreply.github.com>
2024-05-22 23:28:51 +0530

    Create 838.Palindrome Partitioning.py

---
+++ 838.Palindrome Partitioning.py
@@ -0,0 +1,12 @@
+# Palindrome Partitioning
+
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        ret = []
+        for i in range(1, len(s)+1):
+            if s[:i] == s[i-1::-1]:
+                for rest in self.partition(s[i:]):
+                    ret.append([s[:i]]+rest)
+        if not ret:
+            return [[]]
+        return ret       
