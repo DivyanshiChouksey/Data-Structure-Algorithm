43509217ef6e8e08dc555f0f4c2acf2300b2d1e2
Divyanshi Chouksey <89761026+DivyanshiChouksey@users.noreply.github.com>
2024-09-11 22:52:47 +0530

    Create 909.Minimum Bit Flips to Convert Number.py

---
+++ 909.Minimum Bit Flips to Convert Number.py
@@ -0,0 +1,12 @@
+# Minimum Bit Flips to Convert Number
+
+class Solution:
+    def minBitFlips(self, start: int, goal: int) -> int:
+        # XOR to find differing bits
+        xor_result = start ^ goal
+        count = 0
+        # Brian Kernighans algorithm to count 1s
+        while xor_result:
+            xor_result &= xor_result - 1  # Clear the lowest set bit
+            count += 1
+        return count
